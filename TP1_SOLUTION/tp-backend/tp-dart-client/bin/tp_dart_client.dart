dadbd6c013aab009c204555d1be6b64f3f570d93
Omar Chamsdine <omarchamsdine.for.work@gmail.com>
2025-04-03 21:13:02 +0200

    Add files via upload

---
+++ TP1_SOLUTION/tp-backend/tp-dart-client/bin/tp_dart_client.dart
@@ -0,0 +1,5 @@
+import 'package:tp_dart_client/tp_dart_client.dart' as tp_dart_client;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tp_dart_client.calculate()}!');
+}
---
+++ TP1_SOLUTION/tp-backend/tp-dart-client/test/tp_dart_client_test.dart
@@ -0,0 +1,8 @@
+import 'package:tp_dart_client/tp_dart_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
