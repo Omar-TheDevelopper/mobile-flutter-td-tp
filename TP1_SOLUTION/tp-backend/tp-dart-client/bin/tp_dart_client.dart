7b5530489c065331af85a893a0a8cc7f4cbac988
Omar Chamsdine <omarchamsdine.for.work@gmail.com>
2025-04-03 21:17:46 +0200

    Delete TP1_SOLUTION directory

--- TP1_SOLUTION/tp-backend/tp-dart-client/bin/tp_dart_client.dart
+++
@@ -1,5 +0,0 @@
-import 'package:tp_dart_client/tp_dart_client.dart' as tp_dart_client;
-
-void main(List<String> arguments) {
-  print('Hello world: ${tp_dart_client.calculate()}!');
-}
--- TP1_SOLUTION/tp-backend/tp-dart-client/test/tp_dart_client_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tp_dart_client/tp_dart_client.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
